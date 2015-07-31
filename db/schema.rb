ce850de24b4ff724ce454d7e55fda0ad2e3a76f8
Angel Vanegas <angelman@gmail.com>
2015-07-30 20:09:44 -0400

    passwords

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150730211203) do
+ActiveRecord::Schema.define(version: 20150730234319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,9 +35,12 @@ ActiveRecord::Schema.define(version: 20150730211203) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.boolean  "admin"
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
   end
 
   add_foreign_key "reviews", "produces"
